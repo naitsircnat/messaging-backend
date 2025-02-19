05b91b336e6200a5927be1be6404eea6c7d84340
naitsircnat <cristiangerardchen@gmail.com>
2025-02-19 07:25:55 +0000

    created connection pool module

---
+++ database.js
@@ -0,0 +1,13 @@
+const mysql2 = require("mysql2/promise");
+
+const pool = mysql2.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;
