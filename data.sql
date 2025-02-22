bb8c17e86324b17d366e6121e4edcce173b2272d
naitsircnat <cristiangerardchen@gmail.com>
2025-02-22 06:54:02 +0000

    Added data.sql file for dummy customer data

---
+++ data.sql
@@ -0,0 +1,5 @@
+USE messaging
+
+INSERT INTO users (name, email, password) VALUES ("Tom", "tom@mail.com", 12345678);
+INSERT INTO users (name, email, password) VALUES ("Dick", "dick@mail.com", 12345678);
+INSERT INTO users (name, email, password) VALUES ("Harry", "harry@mail.com", 12345678);
